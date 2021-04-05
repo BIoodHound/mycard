1963ca46930e781546bd805f5c7676782537a4fc
Leo <goncalves_leonardo@yahoo.com>
2021-04-05 12:19:07 +0100

    first commit

---
+++ src/main/java/es/ulpgc/mda/mycard/MycardApplication.java
@@ -0,0 +1,13 @@
+package es.ulpgc.mda.mycard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MycardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/ulpgc/mda/mycard/MycardApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ulpgc.mda.mycard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
